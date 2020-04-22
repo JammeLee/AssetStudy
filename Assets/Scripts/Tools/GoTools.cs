fea1fbe8464cc1eca61eed1baa8f68c37fdbc449
Jamme <jamme_lee@outlook.com>
2020-04-23 01:01:08 +0800

    球形差值

---
+++ Assets/Scripts/Tools/GoTools.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace MFrame.Tools
+{
+    public static class GoTools
+    {
+        /// <summary>
+        /// 以圆心center，做a到b的球形差值。
+        /// </summary>
+        /// <param name="a">起始位置</param>
+        /// <param name="b">目标位置</param>
+        /// <param name="t">单位时间</param>
+        /// <param name="center">圆心</param>
+        /// <returns></returns>
+        public static Vector3 Slerp(Vector3 a, Vector3 b, float t, Vector3 center)
+        {
+            Vector3 res = Vector3.zero;
+            var vA = a - center;
+            var vB = b - center;
+
+            res = Vector3.Slerp(vA, vB, t);
+            res += center;
+
+            return res;
+        }
+    }
+}
