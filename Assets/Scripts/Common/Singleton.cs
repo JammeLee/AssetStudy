965a8134abb306ddfc8dc8399711b3a99eebc5ab
Jamme <jamme_lee@outlook.com>
2020-04-28 22:15:21 +0800

    Add Singleton and MonoSingleton

---
+++ Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace MFrame.Common
+{
+    // Mono单例
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected virtual void Awaking() { }
+        
+        protected virtual void Destroying() {}
+
+        private void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this as T;
+            }
+            else
+            {
+                MLog.E("单例{0}重复出现，已销毁", typeof(T).FullName);
+                Destroy(gameObject);
+            }
+            Awaking();
+        }
+
+        private void OnDestroy()
+        {
+            if (this == Instance)
+            {
+                Destroying();
+                Instance = null;
+            }
+        }
+    }
+
+    public class Singleton<T> where T : class, new()
+    {
+        protected Singleton() { }
+
+        private static T m_Instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (null == m_Instance)
+                {
+                    m_Instance = new T();
+                }
+
+                return m_Instance;
+            }
+        }
+
+        public static void Release()
+        {
+            m_Instance = null;
+        }
+    }
+}
