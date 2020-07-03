4ba92d325f9f741897aa9d6ed00218a9577f8530
Lijiaming <lijiaming1@me-game.com>
2020-07-03 20:17:14 +0800

    添加额外文件

---
+++ Assets/Lua/Ext/StaticLuaCallbacksPartial.cs
@@ -0,0 +1,19 @@
+﻿using ILuaState = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+namespace XLua
+{
+    public static class StaticLuaCallbacksPartial
+    {
+        public static int loadfile(ILuaState L)
+        {
+            string name = L.ToString(1);
+            var oldTop = L.GetTop();
+            L.PushErrorFunc();
+//            if (L.Load)
+//            {
+//                
+//            }
+            return 0;
+        }
+    }
+}
---
+++ Assets/Lua/Ext/TableAPI.cs
@@ -0,0 +1,42 @@
+﻿using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+using ILuaState = System.IntPtr;
+
+
+public static class TableAPI
+{
+    public const string META_CLASS = "__name";
+
+    public static void SetDict(this ILuaState self, string key, float value)
+    {
+        self.PushString(key);
+        self.PushNumber(value);
+        self.RawSet(-3);
+    }
+    
+    public static void SetDict(this ILuaState self, string key, string value)
+    {
+        self.PushString(key);
+        self.PushString(value);
+        self.RawSet(-3);
+    }
+    
+    public static void SetDict(this ILuaState self, string key, bool value)
+    {
+        self.PushString(key);
+        self.PushBooleean(value);
+        self.RawSet(-3);
+    }
+    
+    public static void SetDict(this ILuaState self, string key, LuaCSFunction value)
+    {
+        self.PushString(key);
+        self.PushCSharpFunction(value);
+        self.RawSet(-3);
+    }
+
+//    public static float GetNumber(this ILuaState self, int index, string key, float def = 0)
+//    {
+//        self.GetField(index, key);
+//        var ret = self.opt
+//    }
+}
